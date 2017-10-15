53b42e81fdcb302cf29b0d4f8dbf23f03b9049f8
Bjorn S. von Ubisch <bs@vonubisch.com>
2017-10-15 19:53:41 +0200

    Fix Dialogs ESlint errors

--- src/containers/Dialogs.js
+++ src/containers/Dialogs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import ons from 'onsenui';
 
 import {
@@ -36,7 +36,7 @@ class Dialogs extends React.Component {
     renderToolbar() {
         return (
             <Toolbar>
-                <div className='center'>Dialogs</div>
+                <div className="center">Dialogs</div>
             </Toolbar>
         );
     }
@@ -44,27 +44,56 @@ class Dialogs extends React.Component {
     render() {
         return (
             <Page renderToolbar={this.renderToolbar}>
-                <List dataSource={[< ListItem tappable onClick = {
-                        this.toggleDialog.bind(this)
-                    } > Show dialog({
-                        this.state.dialogOpen
-                            ? 'open'
-                            : 'closed'
-                    }) < /ListItem>, <ListItem onClick={this.showPopovers.bind(this)}> Popovers </ListItem >]} renderHeader={() => <ListHeader>Dialogs</ListHeader>} renderRow={(row) => row}/>
-
-                <List dataSource={[ < ListItem tappable onClick = {
-                        ons.notification.alert.bind(null, 'Hello, world!')
-                    } > Alert dialog < /ListItem>, <ListItem tappable onClick={ons.notification.confirm.bind(null, { message: 'Do you like React?', buttonLabels: ['Yes!', 'Of course!'] })}> Confirmation dialog </ListItem >, < ListItem tappable onClick = {
-                        ons.notification.prompt.bind(null, {message: 'What is your name?'})
-                    } > Prompt dialog < /ListItem> ]} renderHeader={() => <ListHeader>Notifications</ListHeader>} renderRow={(row) => row}/>
+                <List
+                    dataSource={[
+                        <ListItem
+                            key={0}
+                            tappable
+                            onClick={this.toggleDialog.bind(this)}
+                        >
+                            Show dialog
+                            ({this.state.dialogOpen ? 'open' : 'closed'})
+                        </ListItem>,
+                        <ListItem
+                            key={1}
+                            onClick={this.showPopovers.bind(this)}
+                        >
+                            Popovers
+                        </ListItem>
+                    ]}
+                    renderHeader={() => <ListHeader>Dialogs</ListHeader>}
+                    renderRow={(row) => row}
+                />
+                <List
+                    dataSource={[
+                        <ListItem
+                            key={0}
+                            tappable
+                            onClick={ons.notification.alert.bind(null, 'Hello, world!')}
+                        >
+                            Alert dialog
+                        </ListItem>,
+                        <ListItem
+                            key={1}
+                            tappable
+                            onClick={ons.notification.confirm.bind(null, { message: 'Do you like React?', buttonLabels: ['Yes!', 'Of course!'] })}
+                        >
+                            Confirmation dialog
+                        </ListItem>,
+                        <ListItem
+                            key={2}
+                            tappable
+                            onClick={ons.notification.prompt.bind(null, {message: 'What is your name?'})}
+                        >
+                            Prompt dialog
+                        </ListItem>
+                    ]}
+                    renderHeader={() => <ListHeader>Notifications</ListHeader>} renderRow={(row) => row}
+                />
 
                 <Dialog isOpen={this.state.dialogOpen} onCancel={this.toggleDialog.bind(this)} cancelable>
-                    <p style={{
-                        textAlign: 'center'
-                    }}>I am a dialog!</p>
-                    <p style={{
-                        textAlign: 'center'
-                    }}>
+                    <p style={{textAlign: 'center'}}>I am a dialog!</p>
+                    <p style={{textAlign: 'center'}}>
                         <Button disabled={!this.state.dialogOpen} onClick={this.toggleDialog.bind(this)}>Close me!</Button>
                     </p>
                 </Dialog>
@@ -72,5 +101,9 @@ class Dialogs extends React.Component {
         );
     }
 }
+
+Dialogs.propTypes = {
+    navigator: PropTypes.object,
+};
 
 export default Dialogs;
